c54996a9721b82ab72dd5c30dee2f4931ee2dcee
DmitryKurtaev <dmitry.kurtaev@gmail.com>
2016-02-11 17:53:22 +0300

    Replaced test for LN set limit

--- test/test_bijective_checker/test_bijective_checker.cc
+++ test/test_bijective_checker/test_bijective_checker.cc
@@ -23,33 +23,6 @@ void StateMachineOfAllWords(int n_words, StateMachine& state_machine) {
                               AlphabeticEncoder::kEndCharacterId);
 }
 
-// This test for checking LN set limit correctness
-// (outside it McMillan's equation is false).
-TEST(BijectiveChecker, LN_set_limit) {
-  static const unsigned kNumberGenerations = 100;
-
-  std::vector<std::string> code;
-  for (unsigned M = 3; M <= 6; ++M) {
-    unsigned N_max = CodeGenerator::MaxNumberElemCodes(M);
-    for (unsigned N = 2; N <= N_max; ++N) {
-      unsigned L_min = CodeGenerator::MinCodeLength(M, N);
-      unsigned L_max = CodeGenerator::GetLNSetLimit(M, N);
-      for (unsigned L = L_min; L < L_max; ++L) {
-        for (unsigned gen = 0; gen < kNumberGenerations; ++gen) {
-          CodeGenerator::GenCode(L, M, N, code);
-          unsigned sum = 0;
-          for (unsigned i = 0; i < N; ++i) {
-            unsigned len = code[i].length();
-            ASSERT_LE(len, M);
-            sum += 1 << (M - len);
-          }
-          ASSERT_GT(sum, 1 << M);
-        }
-      }
-    }
-  }
-}
-
 // This test for checking not bijective for codes of all words from LN set.
 // (LN set - set of parameters L and N where code garanted does not satisfy
 // McMillan's condition).
--- test/test_code_generator/test_code_generator.cc
+++ test/test_code_generator/test_code_generator.cc
@@ -61,3 +61,30 @@ TEST(CodeGenerator, elem_codes_uniqueness) {
     }
   }
 }
+
+// This test for checking LN set limit correctness
+// (outside it McMillan's equation is false).
+TEST(BijectiveChecker, LN_set_limit) {
+  static const unsigned kNumberGenerations = 100;
+
+  std::vector<std::string> code;
+  for (unsigned M = 3; M <= 6; ++M) {
+    unsigned N_max = CodeGenerator::MaxNumberElemCodes(M);
+    for (unsigned N = 2; N <= N_max; ++N) {
+      unsigned L_min = CodeGenerator::MinCodeLength(M, N);
+      unsigned L_max = CodeGenerator::GetLNSetLimit(M, N);
+      for (unsigned L = L_min; L < L_max; ++L) {
+        for (unsigned gen = 0; gen < kNumberGenerations; ++gen) {
+          CodeGenerator::GenCode(L, M, N, code);
+          unsigned sum = 0;
+          for (unsigned i = 0; i < N; ++i) {
+            unsigned len = code[i].length();
+            ASSERT_LE(len, M);
+            sum += 1 << (M - len);
+          }
+          ASSERT_GT(sum, 1 << M);
+        }
+      }
+    }
+  }
+}
