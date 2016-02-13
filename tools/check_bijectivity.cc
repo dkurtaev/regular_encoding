10ba505cb19d86f604c748a00cec538a777d654c
DmitryKurtaev <dmitry.kurtaev@gmail.com>
2016-02-13 15:38:36 +0300

    Added tools directory

---
+++ tools/check_bijectivity.cc
@@ -0,0 +1,44 @@
+// This tool uses input file with encoding data
+// and check it bijectiivity. Additinonally, saves visualization
+// of code's state machine and deficits state machine.
+// Flags:
+// [-i] Input file with encoding scheme. See format in alphabetic_encoder.cc
+// [-o] Output directory to save .dot files with code's state machine, deficits
+//      state machine and .log file with conclusion about bijectivity.
+
+#include <string>
+#include <iostream>
+
+#include "include/alphabetic_encoder.h"
+
+std::string FindArg(const std::string& flag, int argc, char** argv);
+
+int main(int argc, char** argv) {
+  std::string input_file = FindArg("-i", argc, argv);
+  std::string outdir = FindArg("-o", argc, argv);
+
+  if (input_file == "") {
+    std::cout << "Please, set input file with flag [-i]" << std::endl;
+    return 0;
+  }
+
+  AlphabeticEncoder encoder(input_file);
+  if (outdir != "") {
+    encoder.CheckBijective();
+    encoder.WriteCodeStateMachine(outdir + "code_state_machine.dot");
+    encoder.WriteDeficitsStateMachine(outdir + "deficits_state_machine.dot");
+  }
+}
+
+std::string FindArg(const std::string& flag, int argc, char** argv) {
+  for (int i = 1; i < argc; ++i) {
+    if (std::string(argv[i]) == flag) {
+      if (i + 1 < argc) {
+        return std::string(argv[i + 1]);
+      } else {
+        return "";
+      }
+    }
+  }
+  return "";
+}
