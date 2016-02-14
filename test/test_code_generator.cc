ec8f1dbeeae36ab4838c96167366155c6656d7c4
DmitryKurtaev <dmitry.kurtaev@gmail.com>
2016-02-14 16:42:25 +0300

    Added basic tests for prefix codes

--- test/test_code_generator.cc
+++ test/test_code_generator.cc
@@ -4,62 +4,51 @@
 
 #include "include/code_generator.h"
 
+static const int kNumberGenerations = 25;
+
+#define GEN_LOOP(M, N, L) \
+for (int M = 2; M <= 6; ++M) { \
+  int N_max = CodeGenerator::MaxNumberElemCodes(M); \
+  for (int N = 2; N <= N_max; ++N) { \
+    int L_min = CodeGenerator::MinCodeLength(M, N); \
+    int L_max = CodeGenerator::MaxCodeLength(M, N); \
+    for (int L = L_min; L <= L_max; ++L) { \
+      for (int gen = 0; gen < kNumberGenerations; ++gen)
+
+#define END_GEN_LOOP }}}
+
 TEST(CodeGenerator, gen_code_lengths) {
-  static const int kNumberGenerations = 25;
-
   std::vector<std::string> code;
-  for (int M = 3; M <= 6; ++M) {
-    int N_max = CodeGenerator::MaxNumberElemCodes(M);
-    for (int N = 2; N <= N_max; ++N) {
-      int L_min = CodeGenerator::MinCodeLength(M, N);
-      int L_max = CodeGenerator::MaxCodeLength(M, N);
-      for (int L = L_min; L <= L_max; ++L) {
-        for (int gen = 0; gen < kNumberGenerations; ++gen) {
-          CodeGenerator::GenCode(L, M, N, code);
+  GEN_LOOP(M, N, L) {
+    CodeGenerator::GenCode(L, M, N, code);
 
-          ASSERT_EQ(code.size(), N);
-          int max_length = 0;
-          int code_length = 0;
-          for (int i = 0; i < code.size(); ++i) {
-            int length = code[i].length();
-            code_length += length;
-            if (length > max_length) {
-              max_length = code[i].length();
-            }
-          }
-          ASSERT_EQ(code_length, L);
-          ASSERT_EQ(max_length, M);
-        }
+    ASSERT_EQ(code.size(), N);
+    int max_length = 0;
+    int code_length = 0;
+    for (int i = 0; i < code.size(); ++i) {
+      int length = code[i].length();
+      code_length += length;
+      if (length > max_length) {
+        max_length = code[i].length();
       }
     }
+    ASSERT_EQ(code_length, L);
+    ASSERT_EQ(max_length, M);
   }
+  END_GEN_LOOP
 }
 
 TEST(CodeGenerator, elem_codes_uniqueness) {
-  static const int kNumberGenerations = 25;
-
   std::vector<std::string> code;
-  for (int M = 3; M <= 6; ++M) {
-    int N_max = CodeGenerator::MaxNumberElemCodes(M);
-    for (int N = 2; N <= N_max; ++N) {
-      int L_min = CodeGenerator::MinCodeLength(M, N);
-      int L_max = CodeGenerator::MaxCodeLength(M, N);
-      for (int L = L_min; L <= L_max; ++L) {
-        for (int gen = 0; gen < kNumberGenerations; ++gen) {
-          CodeGenerator::GenCode(L, M, N, code);
-
-          for (int i = 0; i < code.size(); ++i) {
-            for (int j = 0; j < i; ++j) {
-              ASSERT_NE(code[i], code[j]);
-            }
-            for (int j = i + 1; j < code.size(); ++j) {
-              ASSERT_NE(code[i], code[j]);
-            }
-          }
-        }
+  GEN_LOOP(M, N, L) {
+    CodeGenerator::GenCode(L, M, N, code);
+    for (int i = 0; i < code.size(); ++i) {
+      for (int j = i + 1; j < code.size(); ++j) {
+        ASSERT_NE(code[i], code[j]);
       }
     }
   }
+  END_GEN_LOOP
 }
 
 // This test for checking LN set limit correctness
@@ -88,3 +77,39 @@ TEST(CodeGenerator, LN_set_limit) {
     }
   }
 }
+
+TEST(CodeGenerator, prefix_codes_lengths) {
+  std::vector<std::string> code;
+  GEN_LOOP(M, N, L) {
+    CodeGenerator::GenPrefixCode(L, M, N, code);
+
+    ASSERT_EQ(code.size(), N);
+    int max_length = 0;
+    int code_length = 0;
+    for (int i = 0; i < code.size(); ++i) {
+      int length = code[i].length();
+      code_length += length;
+      if (length > max_length) {
+        max_length = code[i].length();
+      }
+    }
+    ASSERT_EQ(code_length, L);
+    ASSERT_EQ(max_length, M);
+  }
+  END_GEN_LOOP
+}
+
+TEST(CodeGenerator, prefix_codes_uniqueness) {
+  std::vector<std::string> code;
+  GEN_LOOP(M, N, L) {
+    CodeGenerator::GenPrefixCode(L, M, N, code);
+    for (int i = 0; i < code.size(); ++i) {
+      for (int j = i + 1; j < code.size(); ++j) {
+        ASSERT_NE(code[i], code[j]);
+      }
+    }
+  }
+  END_GEN_LOOP
+}
+
+// This test for checking that prefix code really prefix.
